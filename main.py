eebf4a7a0a65b9f77cf0cc4333ddcbe9964b57de
LuisRincon16 <lurinconj@unal.edu.co>
2024-11-11 20:22:48 +0000

    23intento

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ class Asiento:
 
 class Auto:
     cantidadCreados = 0
-    def __init__(self, modelo, precio, asientos: list[Asiento], marca, motor, registro):
+    def __init__(self, modelo, precio, asientos, marca, motor, registro):
         self.modelo = modelo
         self.precio = precio
         self.asientos: list[Asiento] = asientos
